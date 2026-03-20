3e86de146305c3a80a084dcc223faf4ac94a164e
Nazgul773 <bennetmc90@gmail.com>
2026-03-20 16:23:26 +0700

    init

---
+++ python/generate_create_tables.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+
+csv_folder = r'D:\Code\Datasets\brazilian_ecommerce_dataset_olist'
+output_file = r'sql\02_create_raw_tables.sql'
+
+sql = "USE OlistDWH;\nGO\n\n"
+
+for file in sorted(os.listdir(csv_folder)):
+    if file.endswith('.csv'):
+        # Tabellenname aus Dateiname ableiten
+        table_name = (file
+            .replace('olist_', '')
+            .replace('_dataset', '')
+            .replace('.csv', ''))
+
+        # Nur Header lesen
+        df = pd.read_csv(
+            os.path.join(csv_folder, file),
+            nrows=0,  # nur Spaltenbezeichner, keine Daten
+            encoding='utf-8'
+        )
+
+        columns = ',\n    '.join([f"[{col}] NVARCHAR(255)" for col in df.columns])
+
+        sql += f"-- {file}\n"
+        sql += f"CREATE TABLE raw.[{table_name}] (\n    {columns}\n);\nGO\n\n"
+
+os.makedirs('sql', exist_ok=True)
+
+with open(output_file, 'w', encoding='utf-8') as f:
+    f.write(sql)
